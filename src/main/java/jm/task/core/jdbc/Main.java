4f4886ad65f05f4f056b58ff8c0b427299fb1afa
Stanislav_Patratiy <blazka2011@yandex.ru>
2024-02-06 23:50:15 +0300

    "реализация задачи"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,36 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        // Создание таблицы User(ов)
+        userService.createUsersTable();
+
+        // Добавление 4 User(ов) в таблицу
+        userService.saveUser("John", "Doe", (byte) 30);
+        System.out.println("User с именем – John добавлен в базу данных");
+        userService.saveUser("Jane", "Doe", (byte) 25);
+        System.out.println("User с именем – Jane добавлен в базу данных");
+        userService.saveUser("Jim", "Beam", (byte) 35);
+        System.out.println("User с именем – Jim добавлен в базу данных");
+        userService.saveUser("Jack", "Daniels", (byte) 40);
+        System.out.println("User с именем – Jack добавлен в базу данных");
+
+        // Получение всех User из базы и вывод в консоль
+        System.out.println("Список всех пользователей:");
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user);
+        }
+
+        // Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+
+        // Удаление таблицы
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/my_dbtest";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+
+    static {
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка при подключении к базе данных");
+        }
+        return connection;
+    }
 }
